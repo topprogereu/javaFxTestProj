f0d3df7d999237f06f66b7db0dc1640ceb0211d9
Pavel <topprogereu@gmail.com>
2018-11-26 16:04:41 +0200

    Add class who worked with arrays for Chart

---
+++ src/sample/ArrayCreator.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class ArrayCreator {
+    private int number_elem;
+    private int [] arrX;
+    private int [] arrFunc;
+
+     ArrayCreator(int n)
+    {
+        number_elem = n;
+    }
+
+    public int[] getArrX() {
+        return arrX;
+    }
+
+    public int[] getArrFunc() {
+        return arrFunc;
+    }
+
+}
