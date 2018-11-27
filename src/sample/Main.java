f7c2d2639e4e8402a9cb504b4754ee7dffece46b
Pasha <topprogereu@gmail.com>
2018-11-27 22:27:21 +0200

    asd

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,17 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.application.Application.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("keks.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("javafx/application/sample/keks.fxml"));
         primaryStage.setTitle("Math helper v1.0");
         primaryStage.setScene(new Scene(root, root.getBoundsInParent().getWidth(), root.getBoundsInParent().getHeight()));
         primaryStage.show();
 
     }
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         launch(args);
     }
 }
